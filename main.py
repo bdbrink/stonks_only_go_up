b5993bf61e2906d1311b689813a97d654da0c37a
bdbrink <brendan.brink@outlook.com>
2023-12-11 16:28:26 -0600

    adding histrocial data from 2 sources

---
+++ main.py
@@ -0,0 +1,17 @@
+import yfinance as yf
+from iexcloud import IEXCloud
+
+# Define ticker and date range for historical data
+ticker = "AMZN"
+start_date = datetime.datetime(2023, 10, 01)
+end_date = datetime.datetime(2023, 12, 11)
+
+# Get historical data from Yahoo
+hist_data = yf.download(ticker, start=start_date, end=end_date)
+
+# Get real-time quote from IEX Cloud (requires API key)
+client = IEXCloud(api_token="YOUR_API_KEY")
+quote = client.quote(ticker)
+
+# Combine historical data with real-time quote
+combined_data = hist_data.append(quote.to_frame().transpose())
