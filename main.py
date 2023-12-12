353fa9304646eb31ff6690d1ada02a9882b7e238
bdbrink <brendan.brink@outlook.com>
2023-12-12 16:14:35 -0600

    adding analyst library

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import yfinance as yf
 from iexcloud import IEXCloud
+import finnhub
 
 # Define ticker and date range for historical data
 ticker = "AMZN"
 start_date = datetime.datetime(2023, 10, 1)
 end_date = datetime.datetime(2023, 12, 11)
+# Initialize Finnhub client
+finnhub_client = finnhub.Client()
 
 # Get historical data from Yahoo
 hist_data = yf.download(ticker, start=start_date, end=end_date)
 
-# Get real-time quote from IEX Cloud (requires API key)
-client = IEXCloud(api_token="YOUR_API_KEY")
+client = IEXCloud(api_token)
 quote = client.quote(ticker)
 
 # Combine historical data with real-time quote
